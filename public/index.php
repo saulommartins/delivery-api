085c32746260dae47543eb14f642dab5f014177a
Saulo Mendes Martins <saulommartins@gmail.com>
2020-07-09 02:34:23 -0300

    depurando erro...

--- public/index.php
+++ public/index.php
@@ -22,7 +22,7 @@ define('LARAVEL_START', microtime(true));
 */
 
 require __DIR__.'/../vendor/autoload.php';
-
+dd(__DIR__.'/../vendor/autoload.php');
 /*
 |--------------------------------------------------------------------------
 | Turn On The Lights
